e6c37eff86e307dd291f9bfc440c519f61be2478
Yohei Iino <y.iino@ht-itc.jp>
2024-02-21 17:46:35 +0900

    法人名を変更しました

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -639,7 +639,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={ui?.chat_logo ? ui.chat_logo : Contoso}
+                                    src={ui?.chat_logo ? ui.chat_logo : 辻本郷ITコンサルティング株式会社}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
